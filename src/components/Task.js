5479ebb09ab9b644228ad3d2062970bcfd681119
florchiexco <florchiexco@gmail.com>
2020-02-28 09:39:08 -0300

    task cambiada a funcion

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,21 @@
 import React from "react";
 
-export default class Task extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        const {name, description, id}= this.props.task;
+const Task = ({task, onChangeState, onEditTask}) => {
         return (
             <>
            <div className="card border-danger mb-3">
                 <div className="card-header bg-transparent border-danger">
-                Tarea N° {id}
+                Tarea N° {task.id}
                 </div>
                 <div className="card-body text-danger">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{description}</p>
-                    <button className="btn btn-outline-success" onClick={()=> this.props.onChangeState()}>Tarea completada</button>
-                    <button className="btn btn-outline-warning" onClick={()=> this.props.onEditTask()}>Editar</button>
+                    <h5 className="card-title">{task.name}</h5>
+                    <p className="card-text">{task.description}</p>
+                    <button className="btn btn-outline-success" onClick={()=> onChangeState()}>Tarea completada</button>
+                    <button className="btn btn-outline-warning" onClick={()=> onEditTask()}>Editar</button>
                 </div>
             </div>
           </>
         );
-    }
 }
+
+export default Task;
